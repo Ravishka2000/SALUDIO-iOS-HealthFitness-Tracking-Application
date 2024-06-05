da8457772b5462e9aa1c055d67dccc7dd96ac020
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2024-06-05 14:54:02 +0530

    add more health info

--- Saludio/HealthKitManager.swift
+++ Saludio/HealthKitManager.swift
@@ -10,6 +10,16 @@ class HealthKitManager: ObservableObject {
 	@Published var activeCalories: Double = 0.0
 	@Published var exerciseMinutes: Double = 0.0
 	@Published var standHours: Double = 0.0
+	@Published var distanceWalkingRunning: Double = 0.0
+	@Published var walkingPace: Double = 0.0
+	@Published var runningPace: Double = 0.0
+	@Published var restingEnergy: Double = 0.0
+	@Published var flightsClimbed: Double = 0.0
+	@Published var walkingSteadiness: Double = 0.0
+	@Published var walkingSpeed: Double = 0.0
+	@Published var walkingStepLength: Double = 0.0
+	@Published var sleepAnalysis: [HKCategorySample] = []
+	@Published var heartRate: Double = 0.0
 	
 	func requestAuthorization() {
 		let readTypes = Set([
@@ -18,112 +28,108 @@ class HealthKitManager: ObservableObject {
 			HKQuantityType.quantityType(forIdentifier: .dietaryWater)!,
 			HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!,
 			HKQuantityType.quantityType(forIdentifier: .appleExerciseTime)!,
-			HKQuantityType.quantityType(forIdentifier: .appleStandTime)!
+			HKQuantityType.quantityType(forIdentifier: .appleStandTime)!,
+			HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+			HKQuantityType.quantityType(forIdentifier: .walkingSpeed)!,
+			HKQuantityType.quantityType(forIdentifier: .walkingStepLength)!,
+			HKQuantityType.quantityType(forIdentifier: .appleWalkingSteadiness)!,
+			HKQuantityType.quantityType(forIdentifier: .flightsClimbed)!,
+			HKQuantityType.quantityType(forIdentifier: .basalEnergyBurned)!,
+			HKQuantityType.quantityType(forIdentifier: .restingHeartRate)!,
+			HKQuantityType.quantityType(forIdentifier: .walkingHeartRateAverage)!,
+			HKCategoryType.categoryType(forIdentifier: .sleepAnalysis)!
 		])
 		
 		healthStore.requestAuthorization(toShare: [], read: readTypes) { (success, error) in
 			if success {
-				self.fetchSteps()
-				self.fetchMindfulness()
-				self.fetchWaterIntake()
-				self.fetchActiveCalories()
-				self.fetchExerciseMinutes()
-				self.fetchStandHours()
+				self.fetchData(for: .stepCount)
+				self.fetchData(for: .mindfulSession)
+				self.fetchData(for: .dietaryWater)
+				self.fetchData(for: .activeEnergyBurned)
+				self.fetchData(for: .appleExerciseTime)
+				self.fetchData(for: .appleStandTime)
+				self.fetchData(for: .distanceWalkingRunning)
+				self.fetchData(for: .walkingSpeed)
+				self.fetchData(for: .walkingStepLength)
+				self.fetchData(for: .appleWalkingSteadiness)
+				self.fetchData(for: .flightsClimbed)
+				self.fetchData(for: .basalEnergyBurned)
+				self.fetchData(for: .restingHeartRate)
+				self.fetchData(for: .sleepAnalysis)
+				self.fetchData(for: .heartRate)
 			}
 		}
 	}
 	
-	func fetchSteps() {
-		let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+	func fetchData(for identifier: HKQuantityTypeIdentifier) {
+		guard let quantityType = HKQuantityType.quantityType(forIdentifier: identifier) else { return }
+		
 		let startDate = Calendar.current.startOfDay(for: Date())
 		let endDate = Date()
-		
 		let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
-		let query = HKStatisticsQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (query, result, error) in
-			guard let result = result, let sum = result.sumQuantity() else { return }
-			let steps = sum.doubleValue(for: HKUnit.count())
+		
+		var options: HKStatisticsOptions = .cumulativeSum
+		switch identifier {
+		case .walkingSpeed, .walkingStepLength, .appleWalkingSteadiness, .restingHeartRate, .heartRate, .walkingHeartRateAverage:
+			options = .discreteAverage
+		default:
+			options = .cumulativeSum
+		}
+		
+		let query = HKStatisticsQuery(quantityType: quantityType, quantitySamplePredicate: predicate, options: options) { (query, result, error) in
+			guard let result = result else { return }
 			DispatchQueue.main.async {
-				self.steps = steps
+				switch identifier {
+				case .stepCount:
+					self.steps = result.sumQuantity()?.doubleValue(for: HKUnit.count()) ?? 0.0
+				case .dietaryWater:
+					self.waterIntake = result.sumQuantity()?.doubleValue(for: HKUnit.literUnit(with: .milli)) ?? 0.0
+				case .activeEnergyBurned:
+					self.activeCalories = result.sumQuantity()?.doubleValue(for: HKUnit.kilocalorie()) ?? 0.0
+				case .appleExerciseTime:
+					self.exerciseMinutes = result.sumQuantity()?.doubleValue(for: HKUnit.minute()) ?? 0.0
+				case .appleStandTime:
+					self.standHours = result.sumQuantity()?.doubleValue(for: HKUnit.hour()) ?? 0.0
+				case .distanceWalkingRunning:
+					self.distanceWalkingRunning = result.sumQuantity()?.doubleValue(for: HKUnit.meter()) ?? 0.0
+				case .walkingSpeed:
+					self.walkingPace = result.averageQuantity()?.doubleValue(for: HKUnit.meter().unitDivided(by: HKUnit.second())) ?? 0.0
+				case .walkingStepLength:
+					self.walkingStepLength = result.averageQuantity()?.doubleValue(for: HKUnit.meter()) ?? 0.0
+				case .appleWalkingSteadiness:
+					self.walkingSteadiness = result.averageQuantity()?.doubleValue(for: HKUnit.percent()) ?? 0.0
+				case .flightsClimbed:
+					self.flightsClimbed = result.sumQuantity()?.doubleValue(for: HKUnit.count()) ?? 0.0
+				case .basalEnergyBurned:
+					self.restingEnergy = result.sumQuantity()?.doubleValue(for: HKUnit.kilocalorie()) ?? 0.0
+				case .restingHeartRate, .walkingHeartRateAverage, .heartRate:
+					self.heartRate = result.averageQuantity()?.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute())) ?? 0.0
+				default:
+					break
+				}
 			}
 		}
 		healthStore.execute(query)
 	}
 	
-	func fetchMindfulness() {
-		let mindfulType = HKCategoryType.categoryType(forIdentifier: .mindfulSession)!
+	func fetchData(for identifier: HKCategoryTypeIdentifier) {
+		guard let categoryType = HKCategoryType.categoryType(forIdentifier: identifier) else { return }
+		
 		let startDate = Calendar.current.startOfDay(for: Date())
 		let endDate = Date()
-		
 		let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
-		let query = HKSampleQuery(sampleType: mindfulType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
+		
+		let query = HKSampleQuery(sampleType: categoryType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
 			guard let samples = samples as? [HKCategorySample] else { return }
-			let totalMinutes = samples.reduce(0) { $0 + $1.endDate.timeIntervalSince($1.startDate) / 60 }
 			DispatchQueue.main.async {
-				self.mindfulnessMinutes = totalMinutes
-			}
-		}
-		healthStore.execute(query)
-	}
-	
-	func fetchWaterIntake() {
-		let waterType = HKQuantityType.quantityType(forIdentifier: .dietaryWater)!
-		let startDate = Calendar.current.startOfDay(for: Date())
-		let endDate = Date()
-		
-		let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
-		let query = HKStatisticsQuery(quantityType: waterType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (query, result, error) in
-			guard let result = result, let sum = result.sumQuantity() else { return }
-			let water = sum.doubleValue(for: HKUnit.literUnit(with: .milli))
-			DispatchQueue.main.async {
-				self.waterIntake = water
-			}
-		}
-		healthStore.execute(query)
-	}
-	
-	func fetchActiveCalories() {
-		let caloriesType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!
-		let startDate = Calendar.current.startOfDay(for: Date())
-		let endDate = Date()
-		
-		let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
-		let query = HKStatisticsQuery(quantityType: caloriesType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (query, result, error) in
-			guard let result = result, let sum = result.sumQuantity() else { return }
-			let calories = sum.doubleValue(for: HKUnit.kilocalorie())
-			DispatchQueue.main.async {
-				self.activeCalories = calories
-			}
-		}
-		healthStore.execute(query)
-	}
-	
-	func fetchExerciseMinutes() {
-		let exerciseType = HKQuantityType.quantityType(forIdentifier: .appleExerciseTime)!
-		let startDate = Calendar.current.startOfDay(for: Date())
-		let endDate = Date()
-		
-		let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
-		let query = HKStatisticsQuery(quantityType: exerciseType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (query, result, error) in
-			guard let result = result, let sum = result.sumQuantity() else { return }
-			let exerciseMinutes = sum.doubleValue(for: HKUnit.minute())
-			DispatchQueue.main.async {
-				self.exerciseMinutes = exerciseMinutes
-			}
-		}
-		healthStore.execute(query)
-	}
-	
-	func fetchStandHours() {
-		let standType = HKQuantityType.quantityType(forIdentifier: .appleStandTime)!
-		let startDate = Calendar.current.startOfDay(for: Date())
-		let endDate = Date()
-		
-		let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
-		let query = HKStatisticsQuery(quantityType: standType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (query, result, error) in
-			guard let result = result, let sum = result.sumQuantity() else { return }
-			let standHours = sum.doubleValue(for: HKUnit.hour())
-			DispatchQueue.main.async {
-				self.standHours = standHours
+				switch identifier {
+				case .mindfulSession:
+					self.mindfulnessMinutes = samples.reduce(0) { $0 + $1.endDate.timeIntervalSince($1.startDate) / 60 }
+				case .sleepAnalysis:
+					self.sleepAnalysis = samples
+				default:
+					break
+				}
 			}
 		}
 		healthStore.execute(query)
--- Saludio/HomeView.swift
+++ Saludio/HomeView.swift
@@ -9,167 +9,209 @@ import HealthKitUI
 import SwiftUI
 
 struct HomeView: View {
-    @ObservedObject var healthKitManager = HealthKitManager()
-
-    @State private var steps: Double = 0.0
-    @State private var mindfulnessMinutes: Double = 0.0
-    @State private var waterIntake: Double = 0.0
-    @State private var activeCalories: Double = 0.0
-    @State private var exerciseMinutes: Double = 0.0
-    @State private var standHours: Double = 0.0
-
-    @State private var showTitle: Bool = false
-
-    private var formattedDate: String = {
-        let today = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .full
-        return dateFormatter.string(from: today)
-    }()
-
-    var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading) {
-                HStack(alignment: .center) {
-                    VStack(alignment: .leading) {
-                        Text("\(formattedDate)")
-                        Text("Summary")
-                            .font(.largeTitle)
-                            .bold()
-                    }
-                    .padding()
-
-                    Spacer()
-
-                    Image("Profile")
-                        .resizable()
-                        .frame(width: 60.0, height: 60.0)
-                        .clipShape(Circle())
-                        .padding()
-                }
-
-                ScrollView {
-                    VStack(alignment: .leading) {
-                        Text("Activity")
-                            .font(.title)
-                            .bold()
-                            .padding()
-
-                        // Summary Card
-                        HStack {
-                            VStack(alignment: .leading, spacing: 10) {
-                                VStack(alignment: .leading, spacing: -5) {
-                                    Text("Steps")
-                                        .font(.subheadline)
-                                        .foregroundColor(Color.gray)
-                                    Text(String(format: "%.1f", steps))
-                                        .fontWeight(.bold)
-                                        .font(.title2)
-                                        .foregroundColor(Color("steps"))
-                                }
-
-                                VStack(alignment: .leading, spacing: -5) {
-                                    Text("Calories")
-                                        .font(.subheadline)
-                                        .foregroundColor(Color.gray)
-                                    Text(String(format: "%.1f", activeCalories))
-                                        .fontWeight(.bold)
-                                        .font(.title2)
-                                        .foregroundColor(Color("calories"))
-                                }
-                                VStack(alignment: .leading, spacing: -5) {
-                                    Text("Exercise")
-                                        .font(.subheadline)
-                                        .foregroundColor(Color.gray)
-                                    Text(String(format: "%.1f", exerciseMinutes))
-                                        .fontWeight(.bold)
-                                        .font(.title2)
-                                        .foregroundColor(Color("exercise"))
-                                }
-                            }
-
-                            Spacer()
-
-                            VStack {
-                                ZStack {
-                                    SummeryRingView(icon: "arrow.up", BG: "steps", WHeight: 50, completionRate: completedStepPercentage(), ringThickness: 20, colorGradient: Gradient(colors: [Color("steps"), Color("steps")]))
-                                    SummeryRingView(icon: "arrow.forward", BG: "calories", WHeight: 100, completionRate: burnedCaloriesPercentage(), ringThickness: 20, colorGradient: Gradient(colors: [Color("calories"), Color("calories")]))
-                                    SummeryRingView(icon: "arrow.forward", BG: "exercise", WHeight: 150, completionRate: completedExercisePercentage(), ringThickness: 20, colorGradient: Gradient(colors: [Color("exercise"), Color("exercise")]))
-                                }
-                                .padding()
-                            }
-                        }
-                        .padding()
+	@ObservedObject var healthKitManager = HealthKitManager()
+	
+	@State private var steps: Double = 0.0
+	@State private var mindfulnessMinutes: Double = 0.0
+	@State private var waterIntake: Double = 0.0
+	@State private var activeCalories: Double = 0.0
+	@State private var exerciseMinutes: Double = 0.0
+	@State private var standHours: Double = 0.0
+	@State private var distanceWalkingRunning: Double = 0.0
+	@State private var walkingPace: Double = 0.0
+	@State private var runningPace: Double = 0.0
+	@State private var restingEnergy: Double = 0.0
+	@State private var flightsClimbed: Double = 0.0
+	@State private var walkingSteadiness: Double = 0.0
+	@State private var walkingSpeed: Double = 0.0
+	@State private var walkingStepLength: Double = 0.0
+	@State private var sleepAnalysis: [HKCategorySample] = []
+	@State private var heartRate: Double = 0.0
+	
+	@State private var showTitle: Bool = false
+	
+	private var formattedDate: String = {
+		let today = Date()
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateStyle = .full
+		return dateFormatter.string(from: today)
+	}()
+	
+	var body: some View {
+		NavigationStack {
+			VStack(alignment: .leading) {
+				HStack(alignment: .center) {
+					VStack(alignment: .leading) {
+						Text("\(formattedDate)")
+						Text("Summary")
+							.font(.largeTitle)
+							.bold()
+					}
+					.padding()
+					
+					Spacer()
+					
+					Image("Profile")
+						.resizable()
+						.frame(width: 60.0, height: 60.0)
+						.clipShape(Circle())
+						.padding()
+				}
+				
+				ScrollView {
+					VStack(alignment: .leading) {
+						Text("Activity")
+							.font(.title)
+							.bold()
+							.padding()
+						
+						// Summary Card
+						HStack {
+							VStack(alignment: .leading, spacing: 10) {
+								VStack(alignment: .leading, spacing: -5) {
+									Text("Move")
+										.font(.subheadline)
+										.foregroundColor(Color.primary)
+									Text(String(format: "%.1f", activeCalories))
+										.fontWeight(.bold)
+										.font(.title2)
+										.foregroundColor(Color("calories")) +
+									Text("CAL")
+										.font(.footnote)
+										.fontWeight(.bold)
+										.foregroundColor(Color("calories"))
+								}
+								
+								VStack(alignment: .leading, spacing: -5) {
+									Text("Exercise")
+										.font(.subheadline)
+										.foregroundColor(Color.primary)
+									Text(String(format: "%.1f", exerciseMinutes))
+										.fontWeight(.bold)
+										.font(.title2)
+										.foregroundColor(Color("exercise")) +
+									Text("MIN")
+										.font(.footnote)
+										.fontWeight(.bold)
+										.foregroundColor(Color("exercise"))
+								}
+								VStack(alignment: .leading, spacing: -5) {
+									Text("Stand")
+										.font(.subheadline)
+										.foregroundColor(Color.primary)
+									Text(String(format: "%.1f", standHours))
+										.fontWeight(.bold)
+										.font(.title2)
+										.foregroundColor(Color("stand")) +
+									Text("HRS")
+										.font(.footnote)
+										.fontWeight(.bold)
+										.foregroundColor(Color("stand"))
+								}
+							}
+							
+							Spacer()
+							
+							VStack {
+								ZStack {
+									SummeryRingView(icon: "arrow.up", BG: "stand", WHeight: 50, completionRate: completedStandingPercentage(), ringThickness: 20, colorGradient: Gradient(colors: [Color("stand"), Color("stand")]))
+									SummeryRingView(icon: "arrow.forward.to.line", BG: "exercise", WHeight: 100, completionRate: completedExercisePercentage(), ringThickness: 20, colorGradient: Gradient(colors: [Color("exercise"), Color("exercise")]))
+									SummeryRingView(icon: "arrow.forward", BG: "calories", WHeight: 150, completionRate: burnedCaloriesPercentage(), ringThickness: 20, colorGradient: Gradient(colors: [Color("calories"), Color("calories")]))
+								}
+								.padding()
+							}
+						}
+						.padding()
 						.background(.bg)
-                        .cornerRadius(15)
-                        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 5)
-                        .padding(.horizontal)
-
-                        VStack {
-                            HStack {
-                                Text("Steps")
-                                Spacer()
-                                Text("Distance")
-                            }
-                            HStack {
-                                Text("Steps")
-                                Spacer()
-                                Text("Distance")
-                            }
-                            HStack {
-                                Text("Steps")
-                                Spacer()
-                                Text("Distance")
-                            }
-                            HStack {
-                                Text("Steps")
-                                Spacer()
-                                Text("Distance")
-                            }
-                        }
-                        .padding()
+						.cornerRadius(15)
+						.shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 5)
+						.padding(.horizontal)
+						
+						// New UI elements for additional data
+						VStack(alignment: .leading, spacing: 10) {
+							Group {
+								DataRowView(title: "Distance Walking/Running", value: distanceWalkingRunning, unit: "m")
+								DataRowView(title: "Walking Pace", value: walkingPace, unit: "m/s")
+								DataRowView(title: "Running Pace", value: runningPace, unit: "m/s")
+								DataRowView(title: "Resting Energy", value: restingEnergy, unit: "kcal")
+								DataRowView(title: "Flights Climbed", value: flightsClimbed, unit: "floors")
+								DataRowView(title: "Walking Steadiness", value: walkingSteadiness, unit: "%")
+								DataRowView(title: "Walking Speed", value: walkingSpeed, unit: "m/s")
+								DataRowView(title: "Walking Step Length", value: walkingStepLength, unit: "m")
+								DataRowView(title: "Heart Rate", value: heartRate, unit: "bpm")
+								DataRowView(title: "Mindfulness Minutes", value: mindfulnessMinutes, unit: "min")
+								DataRowView(title: "Water Intake", value: waterIntake, unit: "ml")
+								DataRowView(title: "Stand Hours", value: standHours, unit: "hrs")
+							}
+						}
+						.padding()
 						.background(.bg)
-                        .cornerRadius(15)
-                        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 5)
-                        .padding(.horizontal)
-                    }
-                }
-            }
-        }
-        .onAppear {
-            healthKitManager.requestAuthorization()
-            fetchHealthData()
-        }
-    }
+						.cornerRadius(15)
+						.shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 5)
+						.padding(.horizontal)
+					}
+				}
+			}
+		}
+		.onAppear {
+			healthKitManager.requestAuthorization()
+		}
+		.onChange(of: healthKitManager.steps) { steps = healthKitManager.steps }
+		.onChange(of: healthKitManager.mindfulnessMinutes) { mindfulnessMinutes = healthKitManager.mindfulnessMinutes }
+		.onChange(of: healthKitManager.waterIntake) { waterIntake = healthKitManager.waterIntake }
+		.onChange(of: healthKitManager.activeCalories) { activeCalories = healthKitManager.activeCalories }
+		.onChange(of: healthKitManager.exerciseMinutes) { exerciseMinutes = healthKitManager.exerciseMinutes }
+		.onChange(of: healthKitManager.standHours) { standHours = healthKitManager.standHours }
+		.onChange(of: healthKitManager.distanceWalkingRunning) { distanceWalkingRunning = healthKitManager.distanceWalkingRunning }
+		.onChange(of: healthKitManager.walkingPace) { walkingPace = healthKitManager.walkingPace }
+		.onChange(of: healthKitManager.runningPace) { runningPace = healthKitManager.runningPace }
+		.onChange(of: healthKitManager.restingEnergy) { restingEnergy = healthKitManager.restingEnergy }
+		.onChange(of: healthKitManager.flightsClimbed) { flightsClimbed = healthKitManager.flightsClimbed }
+		.onChange(of: healthKitManager.walkingSteadiness) { walkingSteadiness = healthKitManager.walkingSteadiness }
+		.onChange(of: healthKitManager.walkingSpeed) { walkingSpeed = healthKitManager.walkingSpeed }
+		.onChange(of: healthKitManager.walkingStepLength) { walkingStepLength = healthKitManager.walkingStepLength }
+		.onChange(of: healthKitManager.heartRate) { heartRate = healthKitManager.heartRate }
+		
+	}
+		
+	func completedStepPercentage() -> Double {
+		return (steps / 200)
+	}
+	
+	func burnedCaloriesPercentage() -> Double {
+		return (activeCalories / 200)
+	}
+	
+	func completedExercisePercentage() -> Double {
+		return (exerciseMinutes / 200)
+	}
+	
+	func completedStandingPercentage() -> Double {
+		return (standHours / 200)
+	}
+}
 
-    func fetchHealthData() {
-        steps = healthKitManager.steps
-        mindfulnessMinutes = healthKitManager.mindfulnessMinutes
-        waterIntake = healthKitManager.waterIntake
-        activeCalories = healthKitManager.activeCalories
-        exerciseMinutes = healthKitManager.exerciseMinutes
-        standHours = healthKitManager.standHours
-    }
-
-    func completedStepPercentage() -> Double {
-        return (steps / 200)
-    }
-
-    func burnedCaloriesPercentage() -> Double {
-        return (activeCalories / 200)
-    }
-
-    func completedExercisePercentage() -> Double {
-        return (exerciseMinutes / 200)
-    }
-
-    func completedStandingPercentage() -> Double {
-        return (standHours / 200)
-    }
+struct DataRowView: View {
+	var title: String
+	var value: Double
+	var unit: String
+	
+	var body: some View {
+		HStack {
+			Text(title)
+				.font(.subheadline)
+				.foregroundColor(Color.gray)
+			Spacer()
+			Text(String(format: "%.1f", value) + " " + unit)
+				.fontWeight(.bold)
+				.font(.title2)
+				.foregroundColor(Color.primary)
+		}
+	}
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+	static var previews: some View {
+		HomeView()
+	}
 }
